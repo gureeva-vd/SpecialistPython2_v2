7b1e08111ba7e4972c27c14e35d820ea4da9cb7f
gureeva-vd <103522445+gureeva-vd@users.noreply.github.com>
2022-04-12 20:25:52 +0400

    Update and rename Module1/practice/points/04_long_points.py to Модуль1/практика/Очки/04_long_points.py

---
+++ Модуль1/практика/Очки/04_long_points.py
@@ -0,0 +1,19 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def dist_to(self, other_point):
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
+
+
+
+points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+
+point_start = Point(0, 0)
+list_ = []
+for long in points:
+    list_.append(long.dist_to(point_start))
+
+print(list_)
+
+print(f'Координаты наиболее удаленной точки =  {max(list_)}')
